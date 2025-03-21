bbf1a5afa83b19868ac818b95a2f2f17a6516ea5
george1586 <george_ichim101@yahoo.com>
2025-03-21 10:30:03 -0400

    Proba CyberSex

--- API/Program.cs
+++ API/Program.cs
@@ -13,12 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-    app.UseSwaggerUI();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("*"));
 
 app.UseAuthentication();
